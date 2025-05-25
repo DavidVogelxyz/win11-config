f5e6f9f29d858930b65ef5336b1a6631ea296d16
David Vogel <david@davidvogel.xyz>
2025-05-25 16:47:11 -0600

    FEATURE: now able to install 10 "essential" programs with `installs.ps1`

---
+++ installs.ps1
@@ -0,0 +1,29 @@
+# Install Brave Browser
+winget install --silent --accept-source-agreements --accept-package-agreements --id Brave.Brave
+
+# Install Git
+winget install --silent --accept-source-agreements --accept-package-agreements --id Git.Git
+
+# Install Neovim
+winget install --silent --accept-source-agreements --accept-package-agreements --id Neovim.Neovim
+
+# Install OhMyPosh (PowerShell)
+winget install --silent --accept-source-agreements --accept-package-agreements --id JanDeDobbeleer.OhMyPosh
+
+# Install PowerToys
+winget install --silent --accept-source-agreements --accept-package-agreements --id Microsoft.PowerToys
+
+# Install Fastfetch
+winget install --silent --accept-source-agreements --accept-package-agreements --id Fastfetch-cli.Fastfetch
+
+# Install fzf
+winget install --silent --accept-source-agreements --accept-package-agreements --id junegunn.fzf
+
+# Install GlazeWM
+winget install --silent --accept-source-agreements --accept-package-agreements --id glzr-io.glazewm
+
+# Install Magic Wormhole
+winget install --silent --accept-source-agreements --accept-package-agreements --id magic-wormhole.magic-wormhole
+
+# Install ripgrep
+winget install --silent --accept-source-agreements --accept-package-agreements --id BurntSushi.ripgrep.MSVC
