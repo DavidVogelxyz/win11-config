0afecea301f4834198e25475adcb61b40aa6aae2
David Vogel <david@davidvogel.xyz>
2025-05-26 23:47:56 -0600

    UPDATE: added additional programs to `programs/gaming.ps1`

---
+++ programs/gaming.ps1
@@ -0,0 +1,8 @@
+# Install Steam
+#winget install --silent --accept-source-agreements --accept-package-agreements --id Valve.Steam
+
+# Install Discord
+#winget install --silent --accept-source-agreements --accept-package-agreements --id Discord.Discord
+
+# Install Emulation Station
+#winget install --silent --accept-source-agreements --accept-package-agreements --id Emulationstation.Emulationstation
