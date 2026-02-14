d21737bbd0ac770214b2ac53ac1289a3d70b6cfb
David Vogel <david@davidvogel.xyz>
2026-02-14 15:21:49 -0700

    UPDATE: `installs.ps1` - script now loops through a list of programs

--- installs.ps1
+++ installs.ps1
@@ -1,29 +1,16 @@
-# Install Brave Browser
-winget install --silent --accept-source-agreements --accept-package-agreements --id Brave.Brave
+$progs=@(
+    "Git.Git"
+    "Brave.Brave"
+    "glzr-io.glazewm"
+    "JanDeDobbeleer.OhMyPosh"
+    "junegunn.fzf"
+    "Neovim.Neovim"
+    "BurntSushi.ripgrep.MSVC"
+    "Microsoft.PowerToys"
+    #"Fastfetch-cli.Fastfetch"
+    #"magic-wormhole.magic-wormhole"
+)
 
-# Install Git
-winget install --silent --accept-source-agreements --accept-package-agreements --id Git.Git
-
-# Install Neovim
-winget install --silent --accept-source-agreements --accept-package-agreements --id Neovim.Neovim
-
-# Install OhMyPosh (PowerShell)
-winget install --silent --accept-source-agreements --accept-package-agreements --id JanDeDobbeleer.OhMyPosh
-
-# Install PowerToys
-winget install --silent --accept-source-agreements --accept-package-agreements --id Microsoft.PowerToys
-
-# Install Fastfetch
-winget install --silent --accept-source-agreements --accept-package-agreements --id Fastfetch-cli.Fastfetch
-
-# Install fzf
-winget install --silent --accept-source-agreements --accept-package-agreements --id junegunn.fzf
-
-# Install GlazeWM
-winget install --silent --accept-source-agreements --accept-package-agreements --id glzr-io.glazewm
-
-# Install Magic Wormhole
-winget install --silent --accept-source-agreements --accept-package-agreements --id magic-wormhole.magic-wormhole
-
-# Install ripgrep
-winget install --silent --accept-source-agreements --accept-package-agreements --id BurntSushi.ripgrep.MSVC
+foreach ($prog in $progs) {
+    winget install --silent --accept-source-agreements --accept-package-agreements --id $prog
+}
